a5aceadd8cb8f4a112336df34d2a11c9ec919671
Semyon Pupkov <mail@semyonpupkov.com>
2017-06-12 01:13:32 +0500

    Add consul conversations

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,16 +1,16 @@
 module Consul
   class Application < Rails::Application
-    config.action_mailer.default_options = {from: 'no-reply@proposals.votedevin.com'}
+    config.action_mailer.default_options = {from: 'no-reply@proposals.votedevin.com'.freeze}
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address: "localhost",
+      address: "localhost".freeze,
       port: 25,
       openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
     }
 
     config.force_ssl = false
-    config.action_mailer.asset_host = "http://#{Rails.application.secrets.server_name}"
+    config.action_mailer.asset_host = "http://#{Rails.application.secrets.server_name}".freeze
     config.i18n.default_locale = :en
-    config.time_zone = 'America/New_York'
+    config.time_zone = 'America/New_York'.freeze
   end
 end
