003836f769d6cd0359cc1b9da920c54889010f48
Semyon Pupkov <mail@semyonpupkov.com>
2017-06-15 10:26:48 +0500

    change delivery method

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,12 +1,7 @@
 module Consul
   class Application < Rails::Application
     config.action_mailer.default_options = {from: 'no-reply@proposals.votedevin.com'.freeze}
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address: "localhost".freeze,
-      port: 25,
-      openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
-    }
+    config.action_mailer.delivery_method = :sendmail
 
     config.force_ssl = false
     config.action_mailer.asset_host = "http://#{Rails.application.secrets.server_name}".freeze
