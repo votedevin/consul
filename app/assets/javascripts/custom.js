fe14ddf756f505aceeef79a591f35abd005e87ed
Semyon Pupkov <mail@semyonpupkov.com>
2017-07-12 09:57:47 +0500

    Render google map in sidebar

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,3 +5,31 @@
 //
 //
 
+function initMap() {
+  var mapElem = document.getElementById('js-g-map');
+
+  var map = new google.maps.Map(mapElem, {
+    zoom: $(mapElem).data('zoom') || 10,
+    center: {lat: 40.728214, lng: -73.9651237}
+  });
+
+  map.data.loadGeoJson('/map.geojson');
+
+  map.data.setStyle({
+    strokeWeight: 1,
+    strokeOpacity: 0.4,
+    fillOpacity: 0.2
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+
+  map.data.addListener('click', function(event) {
+    console.log(event.feature.getProperty('boro_name'));
+    infowindow.setContent("<div style='width:150px; text-align: center;'>"+event.feature.getProperty("boro_name")+"</div>");
+
+    var anchor = new google.maps.MVCObject();
+    anchor.set("position",event.latLng);
+
+    infowindow.open(map, anchor);
+  });
+}
